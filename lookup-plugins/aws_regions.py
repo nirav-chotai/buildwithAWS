ea97c05741d225b0a6cf87ffb85c4bd1e5365ba9
Nirav Chotai <niravchotai.ce@gmail.com>
2017-12-03 22:03:03 +0530

    Pushing all templates for DevOps on AWS

--- lookup-plugins/aws_regions.py
+++ lookup-plugins/aws_regions.py
@@ -2,7 +2,7 @@
 # this is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# (at your option) any later version
 
 from ansible import errors
 
